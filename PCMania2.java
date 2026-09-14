ec340d2c30ef2a21d28553a1238615360496a8e3
tVillS05 <pablo.santos@ges.inatel.br>
2026-09-14 16:58:25 -0300

    Update PCMania2.java

--- PCMania2.java
+++ PCMania2.java
@@ -1,84 +1,94 @@
+package pcmania;
+
 import java.util.Scanner;
 
-public class PCMania2 {
+public class PCMania {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int matricula = 598;
 
-        float preco1 = matricula;
-        float preco2 = matricula + 1;
-        float preco3 = matricula + 2;
+        float precoPromo1 = matricula;
+        float precoPromo2 = matricula + 1234;
+        float precoPromo3 = matricula + 5678;
 
-        //Apple
-        HardwareBasico proc1 = new HardwareBasico("Pentium Core i5", 2200);
-        HardwareBasico ram1  = new HardwareBasico("Memória RAM", 8);
-        HardwareBasico hd1   = new HardwareBasico("HD", 500);
+        // promoções
+        // apple
+        HardwareBasico hb11 = new HardwareBasico("Processador Core i3", 2200);
+        HardwareBasico hb12 = new HardwareBasico("Memória RAM", 8);
+        HardwareBasico hb13 = new HardwareBasico("HD", 500);
         SistemaOperacional so1 = new SistemaOperacional("macOS Sequoia", 64);
-        Computador apple = new Computador("Apple", preco1, proc1, ram1, hd1, so1);
-        apple.addMemoriaUSB(new MemoriaUSB("Pen-drive", 16));
+        Computador pc1 = new Computador("Apple", precoPromo1, hb11, hb12, hb13, so1);
+        pc1.addMemoriaUSB(new MemoriaUSB("Pen-drive", 16));
 
-        //Samsung
-        HardwareBasico proc2 = new HardwareBasico("Pentium Core i7", 3370);
-        HardwareBasico ram2  = new HardwareBasico("Memória RAM", 16);
-        HardwareBasico hd2   = new HardwareBasico("HD", 1000); // 1Tb
+        // samsung
+        HardwareBasico hb21 = new HardwareBasico("Processador Core i5", 3370);
+        HardwareBasico hb22 = new HardwareBasico("Memória RAM", 16);
+        HardwareBasico hb23 = new HardwareBasico("HD", 1000);
         SistemaOperacional so2 = new SistemaOperacional("Windows 8", 64);
-        Computador samsung = new Computador("Samsung", preco2, proc2, ram2, hd2, so2);
-        samsung.addMemoriaUSB(new MemoriaUSB("Pen-drive", 32));
+        Computador pc2 = new Computador("Samsung", precoPromo2, hb21, hb22, hb23, so2);
+        pc2.addMemoriaUSB(new MemoriaUSB("Pen-drive", 32));
 
-        //Dell
-        HardwareBasico proc3 = new HardwareBasico("Pentium Core i7", 4500);
-        HardwareBasico ram3  = new HardwareBasico("Memória RAM", 32);
-        HardwareBasico hd3   = new HardwareBasico("HD", 2000); // 2Tb
+        // dell
+        HardwareBasico hb31 = new HardwareBasico("Processador Core i7", 4500);
+        HardwareBasico hb32 = new HardwareBasico("Memória RAM", 32);
+        HardwareBasico hb33 = new HardwareBasico("HD", 2000);
         SistemaOperacional so3 = new SistemaOperacional("Windows 10", 64);
-        Computador dell = new Computador("Dell", preco3, proc3, ram3, hd3, so3);
-        dell.addMemoriaUSB(new MemoriaUSB("HD Externo", 1000)); // 1Tb
+        Computador pc3 = new Computador("Dell", precoPromo3, hb31, hb32, hb33, so3);
+        pc3.addMemoriaUSB(new MemoriaUSB("HD Externo", 1000));
 
-        System.out.print("Digite o nome do cliente: ");
-        String nomeCliente = entrada.nextLine();
-        System.out.print("Digite o CPF do cliente: ");
-        String cpfCliente = entrada.nextLine();
+        // cliente
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.print("Digite seu CPF: ");
+        String cpf = sc.nextLine();
 
-        Cliente cliente = new Cliente(nomeCliente, cpfCliente);
+        Cliente cliente = new Cliente(nome, cpf);
 
+        // menu
         int codigo;
-        do {
-            System.out.println("\nPC MANIA - Promoções");
-            System.out.println("1 - Promoção 1 (Apple)");
-            System.out.println("2 - Promoção 2 (Samsung)");
-            System.out.println("3 - Promoção 3 (Dell)");
-            System.out.println("0 - Finalizar compra");
-            System.out.print("Digite o código da promoção desejada: ");
-            codigo = entrada.nextInt();
+        while (true) {
+            System.out.println("Escolha a promocao:");
+            System.out.println("1 - Apple");
+            System.out.println("2 - Samsung");
+            System.out.println("3 - Dell");
+            System.out.println("0 - finalizar compra");
+            System.out.print("Digite o código: ");
+
+            codigo = sc.nextInt();
 
             if (codigo == 1) {
-                cliente.adicionarComputador(apple);
-                System.out.println("PC Apple adicionado ao carrinho!");
+                cliente.comprar(pc1);
             } else if (codigo == 2) {
-                cliente.adicionarComputador(samsung);
-                System.out.println("PC Samsung adicionado ao carrinho!");
+                cliente.comprar(pc2);
             } else if (codigo == 3) {
-                cliente.adicionarComputador(dell);
-                System.out.println("PC Dell adicionado ao carrinho!");
-            } else if (codigo != 0) {
-                System.out.println("Código inválido! Tente novamente.");
+                cliente.comprar(pc3);
+            } else if (codigo == 0) {
+                System.out.println("Compra finalizada!");
+            } else {
+                System.out.println("Código inválido!");
             }
-        } while (codigo != 0);
 
-        System.out.println("\nResumo da compra");
-        cliente.mostraInfo();
-
-        Computador[] pcsComprados = cliente.getComputadores();
-        for (int i = 0; i < cliente.getQtdComputadores(); i++) {
-            pcsComprados[i].mostraPCConfigs();
+            if (codigo == 0) {
+                break;
+            }
         }
 
-        float total = cliente.calculaTotalCompra();
-        System.out.println("Total: R$ " + total);
+        cliente.mostraInfo();
 
-        // Processamento do pedido
-        ProcessarPedido.processar(pcsComprados);
+        float total = 0;
+        Computador[] compras = cliente.getCompra();
 
-        entrada.close();
+        for (int i = 0; i < cliente.getQtdCompras(); i++) {
+            compras[i].mostraPCConfig();
+            total += compras[i].getPreco();
+        }
+
+        System.out.println("Total da compra: R$ " + total);
+
+        System.out.println("\nProcessando pedido...");
+        ProcessarPedido.processar(compras);
+
+        sc.close();
     }
 }
